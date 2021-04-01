e1ced7e95d19c4a3148221b8b6164b205c9eadb0
SinnottTM <tim.sinnott86@gmail.com>
2021-04-01 10:58:48 +0100

    4rd commit, alternative solutions

--- models/traveller.js
+++ models/traveller.js
@@ -24,15 +24,38 @@ Traveller.prototype.calculateTotalDistanceTravelled = function () {
   }, 0)
 };
 
-Traveller.prototype.getUniqueModesOfTransport = function () {
-  let modesOfTransport = [];
-  this.journeys.forEach((journey) => {
-    if (!modesOfTransport.includes(journey.transport)) {
-      modesOfTransport.push(journey.transport);
-    }
-  })
-  return modesOfTransport;
+// A method, used stuart's idea for this one so all credit to him here. Works.
+// Traveller.prototype.getUniqueModesOfTransport = function () {
+//   let modesOfTransport = [];
+//   this.journeys.forEach((journey) => {
+//     if (!modesOfTransport.includes(journey.transport)) {
+//       modesOfTransport.push(journey.transport);
+//     }
+//   })
+//   return modesOfTransport;
+// };
 
+// Vitor's original solution, very clever but the lecturers recommended spreading it out (see below). Works
+// Traveller.prototype.getUniqueModesOfTransport = function () {
+//   return unique = [...new Set(this.journeys.map(item => item.transport))];
+// };
+
+//Vitor's solution spread out by lecturers (see above). Works.
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  const transportModes = this.journeys.map(item => item.transport);
+  const uniqueTransportModesSet = new Set(transportModes); // Set does not allow duplicates
+  // console.log(uniqueTransportModesSet) TO TEST IS THIS LIST CORRECT IN TERMINAL
+  return [...uniqueTransportModesSet]
 };
+
+// Method CodeClan recommended... Gotta say, this feels messy. Works
+// Traveller.prototype.getUniqueModesOfTransport = function () {
+//   return this.journeys.map((journey) => {
+//     return journey.transport;
+//   })
+//   .filter((transport, index, array) => {
+//     return array.indexOf(transport) === index;
+//   })
+// };
 
 module.exports = Traveller;
